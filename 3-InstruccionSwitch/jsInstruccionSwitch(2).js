e4962ef76c20201c269511dd503d01b34a4e4972
aubelelautaro <36264982+aubelelautaro@users.noreply.github.com>
2018-02-16 12:11:46 -0300

    16/02 cuarta clase

--- 3-InstruccionSwitch/jsInstruccionSwitch(2).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(2).js
@@ -1,11 +1,29 @@
-function Mostrar()
-{
-//tomo la edad  
-var mesDelAño = document.getElementById('mes').value;
+function Mostrar() {  
+    var mesDelAño;
 
-alert (mesDelAño);
+    mesDelAño = document.getElementById('mes').value;
 
+    
+    switch (mesDelAño) 
+    {
+        case "Julio":
+        case "Agosto":
+            alert("Abrigate que hace frio");
+            break;
+        case "Septiembre":
+        case "Octubre":
+        case "Noviembre":
+        case "Diciembre":
+            alert("Ya pasamos el frio, ahora calor!")
+            break;
+        case "Enero":
+        case "Febrero":
+        case "Marzo":
+        case "Abril":
+        case "Mayo":
+        case "Junio":
+            alert("Falta para el invierno")
+            break;
+    }
 
-
-
-}//FIN DE LA FUNCIÓN
+}
--- 3-InstruccionSwitch/jsInstruccionSwitch(4).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(4).js
@@ -1,12 +1,24 @@
 function Mostrar()
 {
-//tomo la edad  
-var mesDelAño = document.getElementById('mes').value;
 
-//alert (mesDelAño);
-	
-	
+    var mesDelAño;
 
+    mesDelAño = document.getElementById("mes").value;
 
+    switch (mesDelAño)
+    {
+        case "Febrero":
+            alert ("Tiene 28 días");
+            break;
+        case "Abril":
+        case "Junio":
+        case "Septiembre":
+        case "Noviembre":
+            alert ("Tiene 30 dias");
+            break;
+        default:
+            alert ("Tiene 31 días");
+            break;
+    }
 
-}//FIN DE LA FUNCIÓN
+}
