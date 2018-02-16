d723dcd583a892779bd22217859deae6e5dc1b2e
aubelelautaro <36264982+aubelelautaro@users.noreply.github.com>
2018-02-16 00:13:00 -0300

    Clase 1 y 2, ejercicios del 1 al 10

--- 1-EntradaSalida/jsEntradaSalida-10.js
+++ 1-EntradaSalida/jsEntradaSalida-10.js
@@ -4,5 +4,18 @@ mostrar el importe con un Descuento del 25 %
 en el cuadro de texto "RESULTADO"*/
 function MostrarAumento()
 {
-	
+    var importe;
+    var Descuento;
+    var resultado;
+
+    importe=document.getElementById("importe").value;
+    
+    importe= parseInt(importe);
+
+    Descuento= 0.75;
+
+    resultado= (importe*Descuento);
+
+    document.getElementById("resultado").value=resultado;
+
 }
